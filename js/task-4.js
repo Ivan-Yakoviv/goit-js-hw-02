aca0b64f42aafc64c962ec3a05e9223402306e97
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-03-24 00:46:59 +0200

    fixed task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,24 +1,21 @@
 function getShippingCost(country) {
-  const inputCountry = country.toLowerCase(); 
+  const inputCountry = country.toLowerCase();
+  const normCountry = inputCountry[0].toUpperCase() + inputCountry.slice(1);
   switch (inputCountry) {
     case "china":
-      return "Shipping to China will cost 100 credits";
-break;
+      return `Shipping to ${normCountry} will cost 100 credits`;
     case "chile":
-      return "Shipping to Chile will cost 250 credits";
-break;
-      case "australia":
-      return "Shipping to Australia will cost 170 credits";
-break;
-      case "jamaica":
-      return "Shipping to Jamaica will cost 120 credits";
-break;
+      return `Shipping to ${normCountry} will cost 250 credits`;
+    case "australia":
+      return `Shipping to ${normCountry} will cost 170 credits`;
+    case "jamaica":
+      return `Shipping to ${normCountry} will cost 120 credits`;
     default: 
       return "Sorry, there is no delivery to your country";
   }
 }
 
-console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("ausTralia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
 console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
 console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
